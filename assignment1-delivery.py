9d4a3cd5a7935d5458ed56a5d60cbfb425223c96
Parham Heydari <heidari1377@gmail.com>
2024-05-25 14:12:04 +0200

    ✨ add a burger - add some comment using py doc

--- assignment1-delivery.py
+++ assignment1-delivery.py
@@ -10,10 +10,16 @@ treeImage1 = pygame.image.load('./delivery-images/tree1.png')
 treeImage2 = pygame.image.load('./delivery-images/tree2.png')
 treeImage3 = pygame.image.load('./delivery-images/tree3.png')
 deliveryImage = pygame.image.load('./delivery-images/delivery.png')
+deliveryWithBurgurImage = pygame.image.load('./delivery-images/delivery-with-burger.png')
 
 navigations = [ (0, 1), (0, -1), (1, 0), (-1, 0) ]
 
 class DeliveryEnv(gym.Env):
+    """ initialises the delivery environment
+
+    Args:
+        no args
+    """
     def __init__(self):
         super().__init__()
 
@@ -22,7 +28,8 @@ class DeliveryEnv(gym.Env):
         self.cell_size = 100
         self.action_space = gym.spaces.Discrete(4)
         self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(self.width, self.height, 1))
-        
+        self.customerPosition = (9, 9)
+        self.restourantPosition = (0, 9)
         self.reset()
 
         # pygame setup
@@ -30,12 +37,13 @@ class DeliveryEnv(gym.Env):
         self.screen = pygame.display.set_mode((self.width * self.cell_size, self.height * self.cell_size))
         self.clock = pygame.time.Clock()
 
+
+        """
+         reset the environment and return the initial state and distance to goal
+        """
     def reset(self):
 
         self.deliveryPosition = (0, 0)
-        self.customerPosition = (9, 9)
-        self.restourantPosition = (0, 9)
-
         self.turn = 0
         self.gotFoodFromRestaurant = False
 
@@ -49,6 +57,17 @@ class DeliveryEnv(gym.Env):
         distance_to_goal = self.width + self.height
         return self.deliveryPosition, distance_to_goal
 
+
+        """
+        take a step in the environment using an action including moving the delivery agent, checking if the delivery agent is at the restaurant position, customer position, or out of bounds
+        args:
+            action: int
+        returns:
+            deliveryPosition: tuple
+            reward: float
+            done: bool
+            info: dict
+        """
     def step(self, action):
         # reward is negative for each step
         reward = -0.05
@@ -68,18 +87,18 @@ class DeliveryEnv(gym.Env):
         if self.deliveryPosition == self.restourantPosition:
             self.gotFoodFromRestaurant = True
             print('Got food from restaurant!')
-            reward = 0.5
+            reward += 0.5
 
         # going to customer without food is our hell state
         if self.deliveryPosition == self.customerPosition and not self.gotFoodFromRestaurant:
             print('Hell state!')
-            reward = -1
+            reward += -10
             done = True
 
         # check if the delivery agent is at the customer position
         if self.deliveryPosition == self.customerPosition and self.gotFoodFromRestaurant:
             print('Delivery made!')
-            reward = 1
+            reward += 1
             done = True
         
 
@@ -91,13 +110,28 @@ class DeliveryEnv(gym.Env):
         return self.deliveryPosition, reward, done, info
 
 
-    def renderPicture(self, image, i, j):
+    
+        """
+        a helper function to render a picture in the environment using a given png/svg file in the specified position
+        args:
+            image: pygame.image
+            i: int
+            j: int
+        """
+    def _renderPicture(self, image, i, j):
         pygame.draw.rect(self.screen, (255, 255, 255), (i * self.cell_size, j * self.cell_size, self.cell_size - 1, self.cell_size - 1))    
-        
         scaledImage = pygame.transform.scale(image, (self.cell_size, self.cell_size))
         self.screen.blit(scaledImage, (i * self.cell_size, j * self.cell_size))
-
-    def renderTree(self, i, j, turn=0):
+        pass
+    
+        """
+        a helper function to render a tree in the environment
+        args:
+            i: int
+            j: int
+            turn: int
+        """
+    def _renderTree(self, i, j, turn=0):
         # sorry, I am not good at drawing trees -\_(-_-)_/-
         # but it should feel like it is windy
         treeImages = [treeImage0, treeImage1, treeImage2, treeImage3, treeImage2, treeImage1]
@@ -107,33 +141,43 @@ class DeliveryEnv(gym.Env):
         pygame.draw.rect(self.screen, (255, 255, 255), (i * self.cell_size, j * self.cell_size, self.cell_size - 1, self.cell_size - 1))    
         scaledImage = pygame.transform.scale(randomTree, (self.cell_size - gap, self.cell_size))
         self.screen.blit(scaledImage, (i * self.cell_size + gap / 2, j * self.cell_size))
-
+        pass
+    
+        """
+        close the environment
+        """
     def close(self):
         pygame.quit()
+        pass
+    
 
+        """
+        render the environment, draw the environment, trees, delivery agent, restaurant, customer, and update the screen
+        """
     def render(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.close()
-
         self.screen.fill((200, 200, 200))
-        print('rendering...')
 
         # draw the environment
         for i in range(self.width):
             for j in range(self.height):
                 color = (255, 255, 255)
                 if (i, j) == self.deliveryPosition:
-                    self.renderPicture(deliveryImage, i, j)
+                    if(self.gotFoodFromRestaurant):
+                        self._renderPicture(deliveryWithBurgurImage, i, j)
+                    else:
+                        self._renderPicture(deliveryImage, i, j)
                     continue
                 if (i, j) == self.customerPosition:
-                    self.renderPicture(homeImage, i, j)
+                    self._renderPicture(homeImage, i, j)
                     continue
                 if (i, j) == self.restourantPosition:
-                    self.renderPicture(restaurantImage, i, j)
+                    self._renderPicture(restaurantImage, i, j)
                     continue
                 if (i, j) in self.treesArray:
-                    self.renderTree(i, j, self.turn)
+                    self._renderTree(i, j, self.turn)
                     continue
 
                 pygame.draw.rect(self.screen, color, (i * self.cell_size, j * self.cell_size, self.cell_size - 1, self.cell_size - 1))    
@@ -143,12 +187,31 @@ class DeliveryEnv(gym.Env):
         pygame.display.flip()
         self.clock.tick(6)
 
-        #show the turn
+        #show the turn number on the window title and console
         print(f'Turn: {self.turn}')
         pygame.display.set_caption(f'Turn: {self.turn}')
 
+        pass
 
-def dfs(env, start, goal, path= [], shortest= None, visited = None):
+"""
+end of the DeliveryEnvClass implementation
+start of the usage sample of the DeliveryEnv
+"""
+
+"""
+a dfs function to find the shortest path from start to goal in the environment
+(this part developed in the practical season as Adi suggested, probably not part of the criteria of the assignment, but I thought it would be nice to include it as well)
+args:
+    env: DeliveryEnv
+    start: tuple
+    goal: tuple
+    path: list
+    shortest: list
+    visited: set
+returns:
+    shortest: list
+"""
+def dfs(env, start, goal, path= [], shortest= None, visited = None) -> list:
     x, y = start
     if(start == goal):
         return path
@@ -170,36 +233,28 @@ def dfs(env, start, goal, path= [], shortest= None, visited = None):
     visited.remove(start)
     return shortest
 
+    """
+    a helper function to render an alert message on the screen, and wait for 3 seconds to close it
+    """
+def _renderAlert(env, message):
+    pygame.display.set_caption(message)
+    env.render()
+    pygame.time.wait(3000)
+
 if __name__ == '__main__':
     env = DeliveryEnv()
     env.reset()
 
     # use dfs to find the shortest path to restaurant (complete search!)
-    actions = dfs(env, env.deliveryPosition, env.restourantPosition)
+    actions = dfs(env, env.deliveryPosition, env.restourantPosition) + dfs(env, env.restourantPosition, env.customerPosition)
 
-    print(actions)
+    reward = 0
     for action in actions:
         pos, reward, done, info = env.step(action)
         env.render()
         if done:
             break
-
-    actions = dfs(env, env.restourantPosition, env.customerPosition)
-
-    print(actions)
-    for action in actions:
-        pos, reward, done, info = env.step(action)
-        env.render()
-        if done:
-            break
-    
-    # random actions:
-    # for _ in range(100000):
-    #     pos, reward, done, info = env.step(env.action_space.sample())
-    #     print('Info:', info)
-    #     env.render()
-    #     if done:
-    #         env.close()
+    _renderAlert(env, 'Delivery completed! Reward:' + str(reward))
     env.close()
 
         
